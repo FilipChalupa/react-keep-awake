ce19aee1b5b9f2d83c2da99bc9ab92437fd1474d
Filip Chalupa <chalupa.filip@gmail.com>
2023-01-12 21:31:30 +0100

    Add CI test

--- src/useKeepAwake.ts
+++ src/useKeepAwake.ts
@@ -1,3 +1,20 @@
+import { useEffect } from 'react'
+import { useWakeLock } from 'react-screen-wake-lock'
+
 export const useKeepAwake = (active = true) => {
-	console.log('useKeepAwake', active)
+	const { isSupported, released, request, release } = useWakeLock()
+
+	useEffect(() => {
+		if (!isSupported) {
+			return
+		}
+
+		if (active === released) {
+			if (active) {
+				request()
+			} else {
+				release()
+			}
+		}
+	}, [active, isSupported, release, released, request])
 }
