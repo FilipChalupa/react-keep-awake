116e23e7b43b1aa74c300174e39401137a2fd33a
Filip Chalupa <chalupa.filip@gmail.com>
2025-06-11 14:00:31 +0200

    Remove deprecated prop

--- src/KeepAwake.tsx
+++ src/KeepAwake.tsx
@@ -2,15 +2,13 @@ import type { FunctionComponent } from 'react'
 import { useKeepAwake } from './useKeepAwake'
 
 export interface KeepAwakeProps {
-	active?: boolean // Deprecated
 	disabled?: boolean
 }
 
 export const KeepAwake: FunctionComponent<KeepAwakeProps> = ({
-	active = true,
 	disabled = false,
 }) => {
-	useKeepAwake(!disabled && active)
+	useKeepAwake(!disabled)
 
 	return null
 }
